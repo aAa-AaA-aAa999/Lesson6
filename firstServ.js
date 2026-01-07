1d60ba7162f7f8ec41226f842490bd61a4d97e74
AnnTumanina <ani-tumanina@yandex.ru>
2026-01-07 14:43:04 +0300

    Create first server

---
+++ firstServ.js
@@ -0,0 +1,72 @@
+const path = require("path")
+const fs = require("fs")
+const http = require("http")
+const PORT = 3000
+
+const server = http.createServer(async (request, response)=>{
+    response.setHeader("Content-Type", "text/html; charset=utf8")
+
+    //Главная страница сайта
+    if(request.url==='/'){
+        response.statusCode = 200
+        response.end(`
+            <!DOCTYPE html>
+            <html lang="ru">
+            <body>
+                <h1>Главная страница</h1>
+                <a href="/contact">Контакты</a>
+            </body>
+            </html>
+            `)
+    }
+
+    //Страница контактов
+    else if (request.url==='/contact'){
+        response.statusCode = 200
+        response.end(`
+            <!DOCTYPE html>
+            <html lang="ru">
+            <body>
+                <h1>Ann</h1>
+                <div>
+                <a href="https://github.com/aAa-AaA-aAa999" target="_blank">
+                My github
+                </a>
+
+                </div>
+                <div>
+                <a href="/">Назад</a>
+                </div>
+            </body>
+            </html>
+            `)
+    }
+
+    //Страница с json
+    else if (request.url==='/api/info'){
+        response.statusCode = 200
+        response.setHeader("Content-Type", "application/json")
+        response.end(JSON.stringify({
+            "serverName": "MyPC",
+            "version": "1.0.0",
+            "status": "working"
+        }))
+    }
+
+    //Страница не найдена
+    else {
+            response.statusCode = 404
+            const imgPath = path.join(__dirname, 'rosi.png') 
+            if (fs.existsSync(imgPath)) { 
+                const img = fs.readFileSync(imgPath) 
+                response.setHeader("Content-Type", "image/png")
+                response.end(img) 
+            } 
+            else { 
+                response.end("Страница не найдена") 
+            }
+        }
+})
+
+server.listen(PORT)
+console.log("Серв был запущ")
